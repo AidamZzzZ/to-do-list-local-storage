288b54b6d72a74d4d246f17ccac62a2f2e56d759
AidamZzzZ <adrian_alliskair@hotmail.com>
2025-03-01 22:01:42 -0400

    search task

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
- const addNewTaskInput = document.querySelector(".add-new-task");
+const addNewTaskInput = document.querySelector(".add-new-task");
 const addTaskbutton = document.querySelector(".btn-add-task");
 const tasksList = document.querySelector(".tasks-list");
 const msgEl = document.querySelector(".msg");
 
 const tasks = [];
+
+let taskToLocaleStorage = JSON.parse(localStorage.getItem("tasks")) || [];
 
 addTaskbutton.addEventListener("click", () => addTask());
 
@@ -15,11 +17,23 @@ addNewTaskInput.addEventListener('keydown', (e) => {
 	};
 });
 
+function noTaskAddTask() {
+	if (tasks.length === 0) {
+		msgEl.classList.remove("filter")
+	} else {
+		msgEl.classList.add("filter")
+	}
+}
+
 function addTask() {
 	const task = addNewTaskInput.value;
 	tasks.push({ taskName: task, complete: false });
 	addNewTaskInput.value = "";
 
+	localStorage.setItem('tasks', JSON.stringify(task))
+	console.log(taskToLocaleStorage)
+
+	noTaskAddTask();
 	renderTask();
 };
 
@@ -28,8 +42,15 @@ function renderTask() {
 	tasks.forEach((task, index) => {	
 		const li = document.createElement("li");
 		const nameTask = document.createElement("span");
-		
-		const checkbox = toggleCheckbox(task);
+		const labelCheckbox = document.createElement("label");
+		labelCheckbox.setAttribute("for", "check-input");
+		labelCheckbox.classList.add("custom-checkbox")
+
+		const checkbox = toggleCheckbox(task, nameTask);
+		checkbox.name = "check-input";
+
+		task.complete ? nameTask.classList.add("check") : nameTask.classList.remove("check");
+
 		const deleteBtn = deleteButton(index);
 		const doneBtn = doneButton(nameTask, index);
 		const editBtn = editButton(nameTask, doneBtn);
@@ -39,6 +60,7 @@ function renderTask() {
 
 		li.classList.add("task");
 		li.appendChild(checkbox);
+		li.appendChild(labelCheckbox)
 		li.appendChild(nameTask);
 		li.appendChild(deleteBtn);
 		li.appendChild(editBtn);
@@ -50,11 +72,14 @@ function renderTask() {
 
 function editButton(nameTask, doneBtn) {
 	const button = document.createElement("button");
-	button.textContent = "Editar";
+	button.innerHTML = `
+		<i class="fa-solid fa-pen-to-square"></i>
+	`;
 	button.classList.add("edit-btn");
 
 	button.addEventListener("click", () => {
 		nameTask.contentEditable = true;
+		nameTask.classList.add("edit")
 		doneBtn.classList.remove("filter");
 	});
 
@@ -63,12 +88,15 @@ function editButton(nameTask, doneBtn) {
 
 function doneButton(nameTask, index) {
 	const button = document.createElement("button");
-	button.textContent = "Done";
+	button.innerHTML = `
+		<i class="fa-solid fa-check"></i>
+	`;
 	button.classList.add("filter");
 	button.classList.add("done-btn");
 
 	button.addEventListener("click", () => {
 		nameTask.contentEditable = false;
+		nameTask.classList.remove("edit")
 		button.classList.add("filter");
 		tasks[index].taskName = nameTask.textContent;
 	});
@@ -81,25 +109,41 @@ function deleteButton(index) {
 	button.innerHTML = `
 		<i class="fa-solid fa-trash"></i>
 	`;
+
 	button.classList.add("delete-btn")
 	button.addEventListener("click", (e) => {
 		const value = e.target.parentElement;
-		tasks.splice(tasks[index], 1)
-		tasksList.removeChild(value)
+		tasks.splice(tasks[index], 1);
+		tasksList.removeChild(value);
+		noTaskAddTask();
 	});
 
 	return button;
 };
 
-function toggleCheckbox(task) {
+function toggleCheckbox(task, nameTask) {
 	const checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
 	checkbox.checked = task.complete;
-	checkbox.classList.add("check-input")
+	checkbox.classList.add("check-input");
 
 	checkbox.addEventListener("change", () => {
 		task.complete = checkbox.checked;
+		task.complete ? nameTask.classList.add("check") : nameTask.classList.remove("check");
 	});
 
 	return checkbox;
 };
+
+document.addEventListener("keyup", (e) => {
+
+	if (e.target.matches(".search-task")) {
+		
+		document.querySelectorAll(".task").forEach(task => {
+
+			task.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+			? task.classList.remove("filter") : task.classList.add("filter")
+		
+		});
+	};
+});
